8b72b8747423f61ec953d80c429be659e748d0c9
Kyle Chinick <kylechinick@icloud.com>
2022-06-12 15:54:40 -0700

    Add addContact prototype to AddressBook constructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ function AddressBook() {
   this.contacts = {};
 }
 
+AddressBook.prototype.addContact = function (contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -11,3 +15,11 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function () {
   return this.firstName + ' ' + this.lastName;
 };
+
+// Lesson Tests
+
+let addressBook = new AddressBook();
+let contact = new Contact('Ada', 'Lovelace', '503-555-0100');
+let contact2 = new Contact('Grace', 'Hopper', '503-555-0199');
+addressBook.addContact(contact);
+addressBook.addContact(contact2);
