093e2f110579743a2004bae679176cea2f6ecd2a
Kyle Chinick <kylechinick@icloud.com>
2022-06-12 15:20:01 -0700

    Add new contact using Contact constructor, add fullName prototype to Contact constructor

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ function Contact(firstName, lastName, phoneNumber) {
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
+
+let testContact = new Contact('Ada', 'Lovelace', '503-555-1111');
+
+console.log(testContact);
+
+console.log(testContact.lastName);
+
+Contact.prototype.fullName = function () {
+  return this.firstName + ' ' + this.lastName;
+};
+
+console.log(testContact.fullName());
