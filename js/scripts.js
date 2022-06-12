572842cf251aed83eacde2028b1793a76fb8981f
Kyle Chinick <kylechinick@icloud.com>
2022-06-12 15:35:38 -0700

    Add AddressBook constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
+function AddressBook() {
+  this.contacts = {};
+}
+
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 
-let testContact = new Contact('Ada', 'Lovelace', '503-555-1111');
-
-console.log(testContact);
-
-console.log(testContact.lastName);
-
 Contact.prototype.fullName = function () {
   return this.firstName + ' ' + this.lastName;
 };
-
-console.log(testContact.fullName());
